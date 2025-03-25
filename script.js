9342b74ec2c9fd35ca36899fb4529e652ef174c4
andressacamilo17 <159184328+andressacamilo17@users.noreply.github.com>
2025-03-25 11:19:51 -0300

    25/03

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
-const aumentaFonteBotao = document.getElementByld('aumentar-fonte')
-const diminuiFonteBotao = document.getElementByld('diminuir-fonte')
-let tamanhoAtualFonte= 1;
-aumentaFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte + = 0,1;
-    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
-})
-}
+const aumentarFonteBotao = document.getElementByld('aumentar-fonte');
+const diminuirFonteBotao = document.getElementByld('diminuir-fonte');
 
-)
+let tamanhoAtualFonte = 1;
+
+aumentarFonteBotao.addEventListener('click', function() {
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+})
+diminuirFonteBotao.addEventListener('click', function() {
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+})
+
+})
+
