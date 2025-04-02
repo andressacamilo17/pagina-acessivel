4af09323d80271966c33a9165450656c491daf36
andressacamilo17 <159184328+andressacamilo17@users.noreply.github.com>
2025-04-02 11:12:43 -0300

    02/04

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded',function(){
+const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+botaoAcessibilidade,this.addEventListener('click', function(){
+    botaoAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesAcessibilidade.classList.toggle('apresenta-lista');
+})
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
